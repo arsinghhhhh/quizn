4eaef5b59390ddecc5ed908475b460975364537a
arsinghhhhh <157036264+arsinghhhhh@users.noreply.github.com>
2024-03-03 12:02:47 +0530

    Update question.js

--- question.js
+++ question.js
@@ -58,15 +58,88 @@ let questions = [
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+    {
+     numb: 6,
+     question: "In Java, what keyword is used to indicate that a method will not return any value?",
+     answer: "void",
+     options: [
+       "void",
+       "null",
+       "None",
+       "empty"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "Which of the following is NOT a valid method for defining CSS styles?",
+    answer: " Embedded styles",
+    options: [
+      "Inline styles",
+      "External stylesheets",
+      "Internal styles",
+      " Embedded styles"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which sorting algorithm is considered the most efficient for large datasets?",
+    answer: " Quick Sort",
+    options: [
+      "Bubble Sort",
+      "Quick Sort",
+      "Insertion Sort",
+      "Selection Sort"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What does MVC stand for in the context of web development?",
+    answer: "Model-View-Controller",
+    options: [
+      "Model-View-Controller",
+      "Multi-Variable-Coding",
+      "Most-Viewed-Content",
+      "Mobile-View-Creation"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following is NOT a valid method to handle exceptions in Python?",
+    answer: "",
+    options: [
+      "try-except",
+      "try-finally",
+      "try-catch",
+      "try-else"
+    ]
+  },
+
+ 
+
+  {
+    numb: 11,
+    question: "In object-oriented programming, what is the process of creating an instance of a class called?",
+    answer: "Instantiation",
+    options: [
+      "Declaration",
+      "Initialization",
+      "Instantiation",
+      "Construction"
+    ]
+  },
+
+  
+
+
+
+   
+
+
+
+
+
 ];
